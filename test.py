f867149cd2d384c2545317327944ac75d1976f6f
hsuan-kai <top80324@gmail.com>
2022-09-30 15:38:54 +0800

    Update test.py
    add lin chart

--- test.py
+++ test.py
@@ -37,3 +37,12 @@ start_time = st.slider(
      value=datetime(2020, 1, 1, 9, 30),
      format="MM/DD/YY - hh:mm")
 st.write("Start time:", start_time)
+
+# Example 5
+st.header('Line chart')
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
